36c55322e954ee1b1b24fa556055d2ba85d69026
Vineet <vineetkushwaha6325@gmail.com>
2025-05-19 01:06:33 +0530

    1.0.1 minord edits for vercel

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,4 +1,5 @@
 //this is to store the book details also this will store what the user did to this book - user action could be bookmark like dislike or read
+//model/book.js
 const mongoose = require("mongoose");
 //basically a schmea for mongodb
 const BookSchema = new mongoose.Schema({
@@ -8,7 +9,7 @@ const BookSchema = new mongoose.Schema({
     description: { type: String },
     interactions: [{
         userId: { type: String, required: true },
-        action: { type: String, enum: ['bookmark', 'like', 'dislike', 'read'], required: true },
+        action: { type: String, enum: ['bookmark', 'like', 'dislike', 'read', 'click'], required: true },
         timestamp: { type: Date, default: Date.now },
     }]
 });
