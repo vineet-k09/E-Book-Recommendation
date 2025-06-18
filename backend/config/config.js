55f5a67034b116ef7ab271d6e70df8291b26dbd2
Vineet <vineetkushwaha6325@gmail.com>
2025-06-18 21:17:34 +0530

    N.A

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI); //connects mongoose to mongodb db using that env up there -- havent added env of mongo yet    console.log("MongoDB Connected...");
+        await mongoose.connect(process.env.MONGO_URI); //connects mongoose to mongodb db using that env up there -- havent added env of mongo yet 
+        console.log("MongoDB Connected...");
     } catch (err) {
         console.error(err.message);
         process.exit(1);
